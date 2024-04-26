802289ff66859c88d4dac8e44904e6e86beadad6
Pablov <juan.vasquez@valposystem.com>
2024-04-26 15:55:43 -0400

    Modificaciones

--- alkeWallet/app/src/main/java/com/vasquezsoftwaresolutions/alkewallet/RequestMoney.java
+++ alkeWallet/app/src/main/java/com/vasquezsoftwaresolutions/alkewallet/RequestMoney.java
@@ -1,7 +1,17 @@
-package com.vasquezsoftwaresolutions.alkewallet;/**
- *@autor Pablo
- *@create 18-04-2024 23:02
- *@project alkeWallet
- *@Version 1.0
- */public class RequestMoney {
+package com.vasquezsoftwaresolutions.alkewallet;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @autor Pablo
+ * @create 18-04-2024 23:02
+ * @project alkeWallet
+ * @Version 1.0
+ */
+public class RequestMoney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginsignuppage);
+    }
 }
