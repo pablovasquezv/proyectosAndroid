6f965a513b749d3b7935a5fe59e9c56305f39237
Pablov <juan.vasquez@valposystem.com>
2024-04-28 15:27:51 -0400

    Creación de proyecto fragmento_estaticos

---
+++ fragmentos_estaticos/app/src/main/java/com/vasquezsoftwaresolutions/fragmentos_estaticos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vasquezsoftwaresolutions.fragmentos_estaticos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
